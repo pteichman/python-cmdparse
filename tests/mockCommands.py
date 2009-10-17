f0694b36837759b68539a4527a71a378d3c43cba
Peter Teichman <peter@teichman.org>
2009-10-17 14:32:36 -0400

    Add a test that ensures the right command is created.

---
+++ tests/mockCommands.py
@@ -0,0 +1,8 @@
+from cmdparse import Command
+
+class EchoCommand(Command):
+    def __init__(self):
+        Command.__init__(self, "echo", summary="echo a string")
+
+    def run(self, options, args):
+        print " ".join(args)
--- tests/test_cmdparse.py
+++ tests/test_cmdparse.py
@@ -1,8 +1,20 @@
 import unittest
 from cmdparse import Command, CommandParser
 
+import mockCommands
+
 class testCmdparse(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.parser = CommandParser()
+        self.parser.add_commands(mockCommands)
+
+    def tearDown(self):
+        del self.parser
+
+    def testEcho(self):
+        (command, options, args) = self.parser.parse_args("echo foo".split())
+
+        self.assert_(isinstance(command, mockCommands.EchoCommand))
 
 test_suite = unittest.TestSuite()
 test_suite.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(testCmdparse))
