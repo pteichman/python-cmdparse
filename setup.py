09bf8a7cf2e4a9858126933e03b195a3ea90aea0
Peter Teichman <peter@teichman.org>
2010-03-13 08:14:53 -0500

    Prepare setup information for release to the Python Package Index

--- setup.py
+++ setup.py
@@ -8,7 +8,17 @@ setup(
     name = "cmdparse",
     version = "0.9",
     author = "Peter Teichman",
-    license = "MIT",
-    packages = find_packages(exclude=["tests"]),
+    author_email = "peter@teichman.org",
+    url = "http://wiki.github.com/pteichman/python-cmdparse/",
+    description = "A command line parser for CVS-style subcommands",
+    packages = ["cmdparse"],
     test_suite = "tests.cmdparse_suite",
+    classifiers = [
+        "Programming Language :: Python",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: User Interfaces"
+    ]
 )
