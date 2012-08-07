cd2b5adb9e2cacef2e1214364094f4e1d0957018
Peter Teichman <peter@teichman.org>
2012-08-07 17:15:17 -0400

    Ditch ez_setup altogether.
    It's not necessary in the modern Python world.

--- setup.py
+++ setup.py
@@ -1,12 +1,7 @@
 #!/usr/bin/env python
 
-try:
-    from setuptools import setup, find_packages
-except ImportError:
-    from ez_setup import use_setuptools
-    use_setuptools()
-
 from setuptools import setup, find_packages
+
 setup(
     name = "cmdparse",
     version = "0.9",
