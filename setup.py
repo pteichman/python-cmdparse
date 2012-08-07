7cbff88d9afb38d5216136bbed923505f9bfecda
Peter Teichman <peter@teichman.org>
2012-08-07 17:30:47 -0400

    Add a "check" command to setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,38 @@
 #!/usr/bin/env python
 
-from setuptools import setup, find_packages
+import sys
+
+from setuptools import Command, find_packages, setup
+
+
+class CheckCommand(Command):
+    description = "Run tests."
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        import subprocess
+
+        print "Running pep8..."
+        if subprocess.call(["pep8", "cmdparse", "tests"]):
+            sys.exit("ERROR: failed pep8 checks")
+
+        print "Running pyflakes..."
+        if subprocess.call(["pyflakes", "cmdparse", "tests"]):
+            sys.exit("ERROR: failed pyflakes checks")
+
+        print "Running tests..."
+        if subprocess.call(["coverage", "run", "--source=cmdparse,tests",
+                            "./setup.py", "test"]):
+            sys.exit("ERROR: failed unit tests")
+
+        subprocess.call(['coverage', 'report', '-m'])
+
 
 setup(
     name = "cmdparse",
@@ -9,8 +41,10 @@ setup(
     author_email = "peter@teichman.org",
     url = "http://wiki.github.com/pteichman/python-cmdparse/",
     description = "A command line parser for CVS-style subcommands",
+
     packages = ["cmdparse"],
     test_suite = "tests",
+
     classifiers = [
         "Programming Language :: Python",
         "License :: OSI Approved :: MIT License",
@@ -18,5 +52,9 @@ setup(
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "Topic :: Software Development :: User Interfaces"
-    ]
+    ],
+
+    cmdclass = {
+        "check": CheckCommand
+    }
 )
